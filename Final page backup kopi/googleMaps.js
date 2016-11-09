cebc119c4b45a16395c73bb982fa0d62b0309c03
MagnusMikalsen <magnusemikalsen@hotmail.com>
2016-11-09 17:57:32 +0100

    jadda

---
+++ Final page backup kopi/googleMaps.js
@@ -0,0 +1,11 @@
+function myMap() {
+  var myCenter = new google.maps.LatLng(63.434458, 10.404103);
+  var mapCanvas = document.getElementById("map");
+  var mapOptions = {
+    center: myCenter,
+    zoom: 15}
+  var map = new google.maps.Map(mapCanvas, mapOptions);
+  var marker = new google.maps.Marker({position: myCenter});
+  marker.setMap(map);
+}
+src="https://maps.googleapis.com/maps/api/js?callback=myMap"
